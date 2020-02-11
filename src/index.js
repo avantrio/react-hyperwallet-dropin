920fcf0d91d89b66fa320f1bd7faee2a3ca6a23b
lahiru94 <lahirud.14@cse.mrt.ac.lk>
2020-02-11 17:31:29 +0530

    hyperwallet sdk added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
-
 export default class ExampleComponent extends Component {
   static propTypes = {
     text: PropTypes.string
+  }
+
+  state = {
+    isSdkReady: false
+  }
+
+  componentDidMount() {
+    this.addPaypalSdk()
   }
 
   render() {
@@ -14,9 +20,28 @@ export default class ExampleComponent extends Component {
     } = this.props
 
     return (
-      <div className={styles.test}>
+      <div>
         Example Component: {text}
+        <div id='TransferMethodUI' />
       </div>
     )
+  }
+
+  addPaypalSdk = () => {
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src = `https://sandbox.hyperwallet.com/rest/widgets/transfer-methods/usr-60765e54-4e77-499e-bf5b-8e55d40d9b2e/en.min.js`
+    script.async = true
+    script.onload = () => {
+      this.setState({ isSdkReady: true })
+      window.HWWidgets.initialize((onSuccess, onFailure) => {
+        console.log(onSuccess)
+      })
+    }
+    script.onerror = () => {
+      throw new Error('Hyperwallet SDK could not be loaded.')
+    }
+
+    document.body.appendChild(script)
   }
 }
